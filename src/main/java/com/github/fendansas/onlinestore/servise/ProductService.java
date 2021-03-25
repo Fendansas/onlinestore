8bd85be4ef7aeaeb31ececeefb45f5379f8d2baa
Sergey Danilchik <fendansas@gmail.com>
2021-03-25 21:54:39 +0300

    created ProductService

---
+++ src/main/java/com/github/fendansas/onlinestore/servise/ProductService.java
@@ -0,0 +1,10 @@
+package com.github.fendansas.onlinestore.servise;
+
+import com.github.fendansas.onlinestore.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProducts();
+}
