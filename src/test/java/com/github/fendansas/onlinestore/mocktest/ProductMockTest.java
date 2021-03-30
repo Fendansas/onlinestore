d0647512bb281e9abae2688c9dbe2ec6f2fea50c
Sergey Danilchik <fendansas@gmail.com>
2021-03-30 23:50:40 +0300

    jacoco do not work

--- src/test/java/com/github/fendansas/onlinestore/mocktest/ProductMockTest.java
+++ src/test/java/com/github/fendansas/onlinestore/mocktest/ProductMockTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.parameters.P;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
