b15ab5943e8bbe9e74f6568f52ae0837208de156
Sergey Danilchik <fendansas@gmail.com>
2021-05-02 22:33:24 +0300

    the product is added to the database

--- src/main/java/com/github/fendansas/onlinestore/dto/ProductDTO.java
+++ src/main/java/com/github/fendansas/onlinestore/dto/ProductDTO.java
@@ -1,10 +1,17 @@
 package com.github.fendansas.onlinestore.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
 
     private Integer id;
