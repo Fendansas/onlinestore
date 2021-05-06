46363205df7763112b844e9e1d6f00bb00576265
Sergey Danilchik <fendansas@gmail.com>
2021-05-07 00:45:49 +0300

    fix bug in navBar

--- src/test/java/com/github/fendansas/onlinestore/smoketest/HomeControllerSmokeTest.java
+++ src/test/java/com/github/fendansas/onlinestore/smoketest/HomeControllerSmokeTest.java
@@ -6,8 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,4 +30,13 @@ public class HomeControllerSmokeTest {
     public void contextLoads() throws Exception {
         assertThat(homeController).isNotNull();
     }
+
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string(containsString("Home")));
+    }
+
+
 }
