591c90e16aa92d8ed27de0cd99766e0d199052ac
Sergey Danilchik <fendansas@gmail.com>
2021-05-11 23:48:39 +0300

    AddProductSmokeTest created and fix SecurityConfiguration

--- src/test/java/com/github/fendansas/onlinestore/smoketest/AddProductSmokeTest.java
+++ src/test/java/com/github/fendansas/onlinestore/smoketest/AddProductSmokeTest.java
@@ -1,48 +1,43 @@
 package com.github.fendansas.onlinestore.smoketest;
 
-import com.github.fendansas.onlinestore.controller.HomeController;
+import com.github.fendansas.onlinestore.controller.AddProduct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HomeControllerSmokeTest {
+public class AddProductSmokeTest {
+
+    @Autowired
+    private AddProduct addProduct;
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private HomeController homeController;
 
-    // тест на наличие контекста
     @Test
     public void contextLoads() throws Exception {
-        assertThat(homeController).isNotNull();
+        assertThat(addProduct).isNotNull();
     }
-
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/"))
+        this.mockMvc.perform(get("/addProduct"))
                 .andDo(print()) // пишет лог
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(containsString("Hello, Anonymous!")));
+        .andExpect(content().string(containsString("Enter product details please...")));
     }
-
-
 
 
 }
